aa59955c0ada547318369c3aca15a86958892f62
Igor Oliveira <igorfoliveira@gmail.com>
2019-12-22 17:03:57 -0300

    first commit

---
+++ src/main/java/br/com/ioliveira/SimpleFeignServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.ioliveira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFeignServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleFeignServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ioliveira/SimpleFeignServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ioliveira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleFeignServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
